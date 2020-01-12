b8a50622d86dab151f96e16e89f3b67bd23bb165
minhhoang2404 <au01657286171@gmail.com>
2020-01-12 13:38:25 +0700

    lession01-CODE-07.01.2020

---
+++ lession01-setup-enviroment/src/main/java/j4t/lession01_setup_enviroment/App.java
@@ -0,0 +1,11 @@
+package j4t.lession01_setup_enviroment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
